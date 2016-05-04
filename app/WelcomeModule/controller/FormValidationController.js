387b67416b84aa9c79e92111037ea3fc0cee2103
ollidnanref <ollidnanref@gmail.com>
2016-05-04 20:05:10 -0300

    clase miercoles 04 05 2016

---
+++ app/WelcomeModule/controller/FormValidationController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ollidnanref on 04-05-2016.
+ */
+
+//GENERACIÓN DE MODULO 
+ var app = angularModule('LoginFormModule',[]);
+//(nombre del módulo,arreglo de dependencias)
+
+app.controller('LoginFormController', function ($scope, $http){});
